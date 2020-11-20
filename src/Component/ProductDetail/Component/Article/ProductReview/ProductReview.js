4d1cfa6354847379fce75dcd894bbed3ae88362e
eommoonjoo <eommoonjoo@eommunjuui-MacBookPro.local>
2020-11-20 21:05:05 +0900

    Add: productdetail

---
+++ src/Component/ProductDetail/Component/Article/ProductReview/ProductReview.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import "../../../../ProductDetail/ProductDetail.scss";
+import "../../../../../Styles/common.scss";
+import "../../../../../Styles/reset.scss";
+import ProductReviewImgbox from "../ProductReviewImgbox/ProductReviewImgbox";
+import Slider from "react-slick";
+
+class ProductReview extends Component {
+  render() {
+    const { productdetail } = this.props;
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+    console.log(productdetail);
+    return (
+      <article className="article-product-review">
+        <div className="article-product-review-title">
+          <span className="product-review">상품 후기</span>
+          <span className="product-review-number">285</span>
+        </div>
+        <div className="article-product-review-totalimgbox">
+          <Slider {...settings}>
+            {productdetail.map((product) => (
+              <ProductReviewImgbox
+                key={product.id}
+                id={product.id}
+                contentImg={product.productImg}
+                authorImg={product.authorImg}
+                author={product.author}
+                comment={product.comment}
+              />
+            ))}
+          </Slider>
+          {/* <div className="article-product-review-imgbox">
+            <img className="article-product-review-img"></img>
+            <div className="article-product-review-img-wrapper">
+              <img className="article-product-review-img-logo"></img>
+              <div className="article-product-review-img-id">
+                <div>
+                  <span>은채</span>
+                  <span>예버요</span>
+                </div>
+                <div className="article-product-review-img-like">
+                  <span>좋아요</span>
+                  <span>1,347</span>
+                  <span>댓글</span>
+                  <span>165</span>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </div>
+        <div className="article-product-review-buttonbox">
+          <button className="article-product-review-button">
+            후기 스타일 모두 보기
+          </button>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default ProductReview;
--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Pages/Store/Home/Home";
-import Login from "./Common/Login/Login";
-import SignUp from "./Common/SignUp/SignUp";
-import Ootd from "./Pages/Ootd/Ootd";
-import Cart from "./Common/Cart/Cart";
-import ProductDetail from "./Common/ProductDetail/ProductDetail";
-import SearchPage from "./Common/SearchPage/SearchPage";
-import Unisex from "./Pages/Store/Unisex/Unisex";
+import ProductDetail from "./Component/ProductDetail/ProductDetail";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Ootd" component={Ootd} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/Cart" component={Cart} />
           <Route exact path="/ProductDetail" component={ProductDetail} />
-          <Route exact path="/SearchPage" component={SearchPage} />
-          <Route exact path="/Unisex" component={Unisex} />
         </Switch>
       </Router>
     );
