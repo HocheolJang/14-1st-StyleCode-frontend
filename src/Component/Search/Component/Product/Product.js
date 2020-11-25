3003fb4061eb0903ffc184321b2fbf46a201147e
HocheolJang <daaan0415@gmail.com>
2020-11-25 09:48:38 +0900

    Connect with Backend

--- src/Component/Search/Component/Product/Product.js
+++ src/Component/Search/Component/Product/Product.js
@@ -5,18 +5,35 @@ class Product extends Component {
   constructor() {
     super();
     this.state = {
+      brandList: [],
       productList: [],
+      ootdList: [],
+      userList: [],
+      productCount: 0,
+      ootdCount: 0,
+      userCount: 0,
     };
   }
 
-  componentDidMount() {
-    fetch("10.58.1.162:8000/search?keyword=%EB%82%98%EC%9D%B4%ED%82%A4")
+  getSearchData = () => {
+    fetch("http://10.58.1.162:8000/search?keyword=%EB%82%98%EC%9D%B4%ED%82%A4")
       .then((res) => res.json())
       .then((res) => {
+        console.log(res.results);
         this.setState({
-          productList: res,
+          brandList: res.results.brands,
+          productList: res.results.products,
+          ootdList: res.results.ootds,
+          usersList: res.results.users,
+          productCount: res.results.product_count,
+          ootdCount: res.results.ootd_count,
+          userCount: res.results.user_count,
         });
       });
+  };
+
+  componentDidMount() {
+    this.getSearchData();
   }
 
   render() {
@@ -46,7 +63,7 @@ class Product extends Component {
                     <span className="brandName">{product?.product_brand}</span>
                   </div>
                   <div className="productName">
-                    <span>{product.product_title}</span>
+                    <span>{product?.product_title}</span>
                   </div>
                   <div className="discountPriceBox">
                     <span className="discount">{product?.discount_rate}</span>
