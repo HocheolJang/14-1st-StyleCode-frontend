83d53d6610af00baa28929aca4ff5813f9559d45
HocheolJang <daaan0415@gmail.com>
2020-11-26 14:56:19 +0900

    Refactoring

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Home from "./Pages/Store/Home/Home";
 import CategoryDetail from "./Component/CategoryDetail/CategoryDetail";
 import Ootd from "./Pages/Ootd/Ootd";
-import NavbarOotd from "./Common/Navbar/NavbarOotd/NavbarOotd";
-import NavbarStore from "./Common/Navbar/NavbarStore/NavbarStore";
 import Login from "../src/Component/Login/Login";
 
 class Routes extends React.Component {
@@ -16,8 +15,6 @@ class Routes extends React.Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/categories" component={CategoryDetail} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/NavbarOotd" component={NavbarOotd} />
-          <Route exact path="/NavbarStore" component={NavbarStore} />
           <Route exact path="/Ootd" component={Ootd} />
         </Switch>
       </Router>
